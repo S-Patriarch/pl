6c8348c5eb607146bbcf3171a1496167392df7fe
S-Patriarch <apsk0529-2@mail.ru>
2023-12-19 20:34:28 +0300

    class pl::Timer

---
+++ pltimer.hh
@@ -0,0 +1,62 @@
+/*
+   (c) S-Patriarch, 2023
+   (PL) Patriarch library : pltimer.hh
+
+   Класс pl::Timer для замера времени работы кода.
+
+Компонентные функции:
+
+   start() - начало замера
+   stop()  - завершение замера
+
+   duration_milliseconds() - результат замера в миллисекундах
+   duration_seconds()      - результат замера в секундах
+*/
+
+#ifndef __PL_PLTIMER_HH__
+#define __PL_PLTIMER_HH__
+
+#include <chrono>
+
+namespace pl
+{
+////////////////////////////////////////////////////////////////////////////////
+class Timer
+{
+   std::chrono::time_point<std::chrono::high_resolution_clock> m_startTime;
+   std::chrono::time_point<std::chrono::high_resolution_clock> m_endTime;
+public:
+   Timer(): 
+      m_startTime{std::chrono::high_resolution_clock::now()}
+   {}
+   //---------------------------------------------------------------------------
+   void 
+   start()
+   { 
+      m_startTime=std::chrono::high_resolution_clock::now(); 
+   }
+   //---------------------------------------------------------------------------
+   void 
+   stop()
+   { 
+      m_endTime=std::chrono::high_resolution_clock::now(); 
+   }
+   //---------------------------------------------------------------------------
+   float 
+   duration_milliseconds() const
+   {
+      std::chrono::duration<float,std::milli> duration=m_endTime-m_startTime;
+      return duration.count();
+   }
+   //---------------------------------------------------------------------------
+   float 
+   duration_seconds() const
+   {
+      std::chrono::duration<float> duration=m_endTime-m_startTime;
+      return duration.count();
+   }
+}; // class pl::Timer
+} // namespace pl
+
+#endif // __PL_PLTIMER_HH__
+
