eefeda140bbd27d7f894d3f2990ce26b07b8fac8
S-Patriarch <apsk0529-2@mail.ru>
2023-12-27 16:49:44 +0300

    Оптимизация кода

--- plexcept.hh
+++ plexcept.hh
@@ -1,36 +1,30 @@
 /*
    (c) S-Patriarch, 2023
    (PL) Patriarch library : plexcept.hh
-
    Работа с исключениями.
    Класс наследуется от библиотечного класса - std::exception.
-
-Использование:
-   Пример использования данного класса исключений.
-   В коде программы бросается исключение следующим образом.
-
-   throw pl::Exception("E: Message to exception.");
- 
-   Ловится данное исключение следующим образом.
-
-   try 
-   {
-      throw pl::Exception("E: Message to exception.");
-   }
-   catch(std::exception& ex) 
-   {
-      std::cout 
-         <<"\nПоймано исключение: " 
-         <<ex.what() 
-         <<'\n';
-   }
+   Использование:
+      Пример использования данного класса исключений.
+      В коде программы бросается исключение следующим образом.
+         throw pl::Exception("E: Message to exception.");
+      Ловится данное исключение следующим образом.
+         try 
+         {
+            throw pl::Exception("E: Message to exception.");
+         }
+         catch(std::exception& ex) 
+         {
+            std::cout 
+               <<"\nПоймано исключение: " 
+               <<ex.what() 
+               <<'\n';
+         }
 */
-
 #ifndef PL_PLEXCEPT_HH
 #define PL_PLEXCEPT_HH
-
+//------------------------------------------------------------------------------
 #include <exception>
-
+//------------------------------------------------------------------------------
 namespace pl
 {
 ////////////////////////////////////////////////////////////////////////////////
@@ -44,17 +38,15 @@ public:
    {}
    ~Exception() noexcept 
    {}
-   //---------------------------------------------------------------------------
-   // Перегрузка метода what() стандартного класса std::exception.
-   //
-   virtual 
-   const char* 
-   what() const noexcept override 
+//------------------------------------------------------------------------------
+// Перегрузка метода what() стандартного класса std::exception.
+//
+   virtual const char* what() const noexcept override 
    { 
       return m_message; 
    }
 }; // class pl::Exception
 } // namespace pl
-
+//------------------------------------------------------------------------------
 #endif // PL_PLEXCEPT_HH
 
--- pltimer.hh
+++ pltimer.hh
@@ -1,21 +1,18 @@
 /*
    (c) S-Patriarch, 2023
    (PL) Patriarch library : pltimer.hh
-
    Класс pl::Timer для замера времени работы кода.
-
-Компонентные функции:
-   start() - начало замера
-   stop()  - завершение замера
-   duration_milliseconds() - результат замера в миллисекундах
-   duration_seconds()      - результат замера в секундах
+   Компонентные функции:
+      start() - начало замера
+      stop()  - завершение замера
+      duration_milliseconds() - результат замера в миллисекундах
+      duration_seconds()      - результат замера в секундах
 */
-
 #ifndef PL_PLTIMER_HH
 #define PL_PLTIMER_HH
-
+//------------------------------------------------------------------------------
 #include <chrono>
-
+//------------------------------------------------------------------------------
 namespace pl
 {
 ////////////////////////////////////////////////////////////////////////////////
@@ -27,34 +24,30 @@ public:
    Timer(): 
       m_startTime{std::chrono::high_resolution_clock::now()}
    {}
-   //---------------------------------------------------------------------------
-   void 
-   start()
+//------------------------------------------------------------------------------
+   void start()
    { 
       m_startTime=std::chrono::high_resolution_clock::now(); 
    }
-   //---------------------------------------------------------------------------
-   void 
-   stop()
+//------------------------------------------------------------------------------
+   void stop()
    { 
       m_endTime=std::chrono::high_resolution_clock::now(); 
    }
-   //---------------------------------------------------------------------------
-   float 
-   duration_milliseconds() const
+//------------------------------------------------------------------------------
+   float duration_milliseconds() const
    {
       std::chrono::duration<float,std::milli> duration=m_endTime-m_startTime;
       return duration.count();
    }
-   //---------------------------------------------------------------------------
-   float 
-   duration_seconds() const
+//------------------------------------------------------------------------------
+   float duration_seconds() const
    {
       std::chrono::duration<float> duration=m_endTime-m_startTime;
       return duration.count();
    }
 }; // class pl::Timer
 } // namespace pl
-
+//------------------------------------------------------------------------------
 #endif // PL_PLTIMER_HH
 
