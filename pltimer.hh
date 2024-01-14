78a55c64c3954ef8b719d4881ccea75fa4b609ec
S-Patriarch <134074106+S-Patriarch@users.noreply.github.com>
2024-01-14 22:14:54 +0300

    Класс pl::Timer для замера времени работы кода.

--- pltimer.hh
+++ pltimer.hh
@@ -1,53 +1,49 @@
-/*
-   (c) S-Patriarch, 2023
-   (PL) Patriarch library : pltimer.hh
-   Класс pl::Timer для замера времени работы кода.
-   Компонентные функции:
-      start() - начало замера
-      stop()  - завершение замера
-      duration_milliseconds() - результат замера в миллисекундах
-      duration_seconds()      - результат замера в секундах
-*/
+//
+// (c) 2024 S-Patriarch
+// (PL) Patriarch library : pltimer.hh
+//
+// Класс pl::Timer для замера времени работы кода.
+//
+// Компонентные функции:
+//    start() - начало замера
+//    stop()  - завершение замера
+//    duration_milliseconds() - результат замера в миллисекундах
+//    duration_seconds()      - результат замера в секундах
+//
 #ifndef PL_PLTIMER_HH
 #define PL_PLTIMER_HH
-//------------------------------------------------------------------------------
+
 #include <chrono>
-//------------------------------------------------------------------------------
-namespace pl
-{
-////////////////////////////////////////////////////////////////////////////////
-class Timer
-{
-   std::chrono::time_point<std::chrono::high_resolution_clock> m_startTime;
-   std::chrono::time_point<std::chrono::high_resolution_clock> m_endTime;
-public:
-   Timer(): 
-      m_startTime{std::chrono::high_resolution_clock::now()}
-   {}
-//------------------------------------------------------------------------------
-   void start()
-   { 
-      m_startTime=std::chrono::high_resolution_clock::now(); 
-   }
-//------------------------------------------------------------------------------
-   void stop()
-   { 
-      m_endTime=std::chrono::high_resolution_clock::now(); 
-   }
-//------------------------------------------------------------------------------
-   float duration_milliseconds() const
-   {
-      std::chrono::duration<float,std::milli> duration=m_endTime-m_startTime;
-      return duration.count();
-   }
-//------------------------------------------------------------------------------
-   float duration_seconds() const
-   {
-      std::chrono::duration<float> duration=m_endTime-m_startTime;
-      return duration.count();
-   }
-}; // class pl::Timer
+
+namespace pl {
+   /////////////////////////////////////////////////////////////////////////////
+   class Timer {
+   private:
+      std::chrono::time_point<std::chrono::high_resolution_clock> m_start_time;
+      std::chrono::time_point<std::chrono::high_resolution_clock> m_end_time;
+   public:
+      Timer() : m_start_time{std::chrono::high_resolution_clock::now()} {}
+      void start()
+      { 
+         m_start_time = std::chrono::high_resolution_clock::now(); 
+      }
+      void stop()
+      { 
+         m_end_time = std::chrono::high_resolution_clock::now(); 
+      }
+      float duration_milliseconds() const
+      {
+         std::chrono::duration<float, std::milli> duration = 
+            m_end_time-m_start_time;
+         return duration.count();
+      }
+      float duration_seconds() const
+      {
+         std::chrono::duration<float> duration = m_end_time-m_start_time;
+         return duration.count();
+      }
+   };
 } // namespace pl
-//------------------------------------------------------------------------------
+
 #endif // PL_PLTIMER_HH
 
