1d8b5d922d2ed198082fc1da9fe51612a306cf84
S-Patriarch <134074106+S-Patriarch@users.noreply.github.com>
2024-01-15 22:07:49 +0300

    работа с исключениями

--- plexcept.hh
+++ plexcept.hh
@@ -1,52 +1,39 @@
-/*
-   (c) S-Patriarch, 2023
-   (PL) Patriarch library : plexcept.hh
-   Работа с исключениями.
-   Класс наследуется от библиотечного класса - std::exception.
-   Использование:
-      Пример использования данного класса исключений.
-      В коде программы бросается исключение следующим образом.
-         throw pl::Exception("E: Message to exception.");
-      Ловится данное исключение следующим образом.
-         try 
-         {
-            throw pl::Exception("E: Message to exception.");
-         }
-         catch(std::exception& ex) 
-         {
-            std::cout 
-               <<"\nПоймано исключение: " 
-               <<ex.what() 
-               <<'\n';
-         }
-*/
+//
+// (c) 2024 S-Patriarch
+// Patriarch library : plexcept.hh
+//
 #ifndef PL_PLEXCEPT_HH
 #define PL_PLEXCEPT_HH
-//------------------------------------------------------------------------------
-#include <exception>
-//------------------------------------------------------------------------------
-namespace pl
-{
-////////////////////////////////////////////////////////////////////////////////
-class Exception final: public std::exception
-{
-   const char* m_message{}; // Сообщение для исключения.
-public:
-   Exception(const char* message_): 
-      std::exception(), 
-      m_message{message_} 
-   {}
-   ~Exception() noexcept 
-   {}
-//------------------------------------------------------------------------------
-// Перегрузка метода what() стандартного класса std::exception.
-//
-   virtual const char* what() const noexcept override 
-   { 
-      return m_message; 
-   }
-}; // class pl::Exception
-} // namespace pl
-//------------------------------------------------------------------------------
-#endif // PL_PLEXCEPT_HH
 
+#include <exception>
+
+namespace pl {
+   class Exception final : public std::exception {
+      // работа с исключениями
+      // класс наследуется от библиотечного класса - std::exception
+      //
+      // использование:
+      // в коде программы бросается исключение следующим образом
+      //    throw pl::Exception("E: Message to exception.");
+      // ловится данное исключение следующим образом
+      //    try {
+      //       throw pl::Exception("E: Message to exception.");
+      //    }
+      //    catch (std::exception& ex) {
+      //       std::cout << "\nПоймано исключение: " 
+      //                 << ex.what() 
+      //                 << '\n';
+      //    }
+   private:
+      const char* m_message {};  // сообщение для исключения
+   public:
+      Exception(const char* mess) : std::exception(), m_message{mess} {}
+      ~Exception() noexcept {}
+      virtual const char* what() const noexcept override 
+         // перегрузка метода what() стандартного класса std::exception
+      { 
+         return m_message; 
+      }
+   };
+}
+#endif // PL_PLEXCEPT_HH
