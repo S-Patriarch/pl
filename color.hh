3bb62362ae6cae73bca90eae12c6a150b5cc245a
S-Patriarch <134074106+S-Patriarch@users.noreply.github.com>
2024-08-01 21:17:50 +0300

    оптимизация кода

--- color.hh
+++ color.hh
@@ -90,7 +90,7 @@ namespace pl {
          delete[] m_backgroundColorRegular;
          delete[] m_backgroundColorBold;
       }
-      enum class color 
+      enum color 
       {
          BLACK = 0, RED = 1, GREEN = 2, YELLOW = 3, 
          BLUE = 4, MAGENTA = 5, CYAN = 6, WHITE = 7,
